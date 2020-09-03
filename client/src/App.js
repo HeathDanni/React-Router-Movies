465745c402bc54f557c14016503408f0b52f560c
Heather Beckman <heatherbeckman85@gmail.com>
2020-09-02 18:38:53 -0600

    wrapped links in router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
 
@@ -30,8 +30,10 @@ const App = () => {
     <div>
       <SavedList list={[ /* This is stretch */]} />
       <div>
+        <BrowserRouter>
         <Link to="/">Home</Link>
         <Link to="/movies">Movie</Link>
+        </BrowserRouter>
       </div>
     </div>
   );
